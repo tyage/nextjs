fe4573eb7c7d9a255fcee23dbbe5d73bdf04ac15
tyage <namatyage@gmail.com>
2021-03-05 18:15:07 +0900

    sample response

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,5 +2,14 @@
 
 export default (req, res) => {
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ data: { allPosts: [
+    {
+      "id": "171a70dd-5834-494e-a8dd-e1be2c2c10c9",
+      "title": "ciao",
+      "votes": 0,
+      "url": "https://cua",
+      "createdAt": "2021-03-05T08:12:10.440Z",
+      "__typename": "Post"
+    }
+  ] } })
 }
